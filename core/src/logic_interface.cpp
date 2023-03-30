aa530c47cb529845b564582d6158f00426d45486
Georgiy <goshantiy@gmail.com>
2023-03-30 12:55:21 +0700

    del unnecessary

--- core/src/logic_interface.cpp
+++
@@ -1,9 +0,0 @@
-#include "logic_interface.h"
-logic_interface_namespace::LogicInterface::LogicInterface(QObject *parent) : BaseInterface(parent)
-{}
-
-bool logic_interface_namespace::LogicInterface::initiliaze(
- const QList<QPointer<QObject> > &dependencies)
-{
-    return true;
-}

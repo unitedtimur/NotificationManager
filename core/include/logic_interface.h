d3d9ba5ed2ed4085e120db0fbd6bfb03eaddf395
Georgiy <goshantiy@gmail.com>
2023-03-30 12:54:06 +0700

    add logic interface

--- core/include/logic_interface.h
+++ core/include/logic_interface.h
@@ -1,15 +1,25 @@
 #ifndef LOGIC_INTERFACE_H
 #define LOGIC_INTERFACE_H
+
 #include "base_interface.h"
-namespace logic_interface_namespace {
+
+namespace Core {
+    /*!
+     * \brief Класс, реализующий плагин бизнес-логики(то есть в дальнейшем здесь будут описаны все
+     * методы, классы, которые понадобяться для реализации задачи отображения графических элементов,
+     * а сам LogicInterface будет собран в библиотеку)
+     */
     class LogicInterface : public BaseInterface
     {
         Q_OBJECT
         Q_INTERFACES(BaseInterface)
+
     public:
+        /*!
+         * \brief Виртуальный деструктор с определением в заголовочном файле
+         */
         explicit LogicInterface(QObject *parent = nullptr);
-        virtual bool initiliaze(const QList<QPointer<QObject>> &dependencies);
     };
 }
-Q_DECLARE_INTERFACE(logic_interface_namespace::LogicInterface, "com.сore.LogicInterface")
+Q_DECLARE_INTERFACE(Core::LogicInterface, "com.Core.LogicInterface")
 #endif // LOGIC_INTERFACE_H

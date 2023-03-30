d1989bbd030ee1d1928e54345664d9e45db6e189
Georgiy <goshantiy@gmail.com>
2023-03-30 12:53:55 +0700

    Merge from master: fix naming + spacing

--- core/include/base_interface.h
+++ core/include/base_interface.h
@@ -1,15 +1,33 @@
 #ifndef BASE_INTERFACE_H
 #define BASE_INTERFACE_H
-#include <QObject>
-#include <QPointer>
-class BaseInterface : public QObject
-{
-    Q_OBJECT
 
-public:
-    using QObject::QObject;
-    virtual ~BaseInterface() = default;
-    virtual bool initialize(const QList<QPointer<QObject>> &dependencies) = 0;
-};
-Q_DECLARE_INTERFACE(BaseInterface, "com.сore.BaseInterface")
+#include <QObject>
+
+namespace Core {
+    /*!
+     * \brief Класс, являющийся базовым интерфейсом, от него будут наследоваться плагины логики и
+     * gui
+     */
+    class BaseInterface : public QObject
+    {
+        Q_OBJECT
+
+    public:
+        /*!
+         * \brief Для класса BaseInterface используется базовый конструктор QObject
+         */
+        using QObject::QObject;
+        /*!
+         * \brief Виртуальный деструктор с определением в хедере
+         */
+        virtual ~BaseInterface() = default;
+        /*!
+         * \brief Виртуальный метод, который реализует инициализацию плагинов логики и gui, исходя
+         * из зависимостей библиотек друг от друга
+         */
+        virtual bool initialize(const QList<QPointer<QObject>> &dependencies) = 0;
+    };
+}
+
+Q_DECLARE_INTERFACE(Core::BaseInterface, "com.Core.BaseInterface")
 #endif // BASE_INTERFACE_H

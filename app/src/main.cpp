5c94aa040a428662625be6df4772caf53c76d983
Georgiy <goshantiy@gmail.com>
2023-04-06 19:51:05 +0700

    add notifications
    XDDDDDDDDDDDDD

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,22 +2,30 @@
 #include "core.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "G:/Qt/repos/NotificationManager/plugins/logic/include/notify_model.h"
+#include "C:/Users/PC/Documents/qtProjects/NotificationManager/plugins/logic/include/notify_model.h"
 #include <QQmlEngine>
-
+#include <QQmlContext>
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QGuiApplication app(argc, argv);
-
-    qmlRegisterType<NotificationModel>("com.example.notificationmodel", 1, 0, "notify_model");
-
+    NotificationModel notificationModel;
+    // Добавляем уведомление в модель
+    notificationModel.addNotification("New notification1", "This is a new notification");
+    notificationModel.addNotification("New notification2", "This is a new notification");
+    notificationModel.addNotification("New notification", "This is a new notification");
+    notificationModel.addNotification("New notification", "This is a new notification");
+    notificationModel.addNotification("New notification", "This is a new notification");
+    notificationModel.addNotification("New notification", "This is a new notification");
+    notificationModel.addNotification("New notification", "This is a new notification");
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
+    engine.rootContext()->setContextProperty("Notificationmodel", &notificationModel);
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
 }

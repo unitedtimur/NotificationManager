d0a7747d4dcc33fa620ccee375b82ae89ff8e971
Anton <inamometr1@gmail.com>
2023-04-06 14:46:27 +0700

    zxc

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include "core.h"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "G:/Qt/repos/NotificationManager/plugins/logic/include/notify_model.h"
+#include <QQmlEngine>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "app started" << std::endl;
-    Core::init();
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+    QGuiApplication app(argc, argv);
 
-    // start core
-    return 0;
+    qmlRegisterType<NotificationModel>("com.example.notificationmodel", 1, 0, "notify_model");
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }

63c764fc63c69b632cbbba07ab6110345183b339
MrKarls <mrkarls0077@gmail.com>
2023-04-10 15:21:28 +0700

    add loadPlugin function

--- core/include/core.h
+++ core/include/core.h
@@ -1,5 +1,12 @@
 #pragma once
-
+#include <iostream>
+#include <QString>
+#include <QDir>
+#include <QPluginLoader>
+#include <QMetaClassInfo>
+#include <QCoreApplication>
+#include "base_interface.h"
 namespace Core {
     void init();
+    void loadPlugins();
 }
--- core/src/core.cpp
+++ core/src/core.cpp
@@ -6,4 +6,69 @@ namespace Core {
     {
         std::cout << "Core init" << std::endl;
     }
+
+    void loadPlugins()
+    {
+
+    #ifdef Q_OS_WIN64
+        const auto pluginExtension = ".dll";
+    #elif defined(Q_OS_LINUX)
+        const auto pluginExtension = ".so";
+    #endif
+        QString parentDir(QDir::currentPath());
+        QDir currentDir(parentDir);
+        currentDir.cdUp();
+        QString projectDir = currentDir.absolutePath();
+
+        QDir pluginsDir(projectDir);
+        pluginsDir.cd("plugins");
+
+        if (!pluginsDir.exists())
+            throw std::runtime_error("Directories do not exist");
+
+        QStringList pluginsFiles = pluginsDir.entryList(QStringList()<<pluginExtension,QDir::Files);
+
+        QList<QPointer<QObject>> dependencies;
+
+          foreach(const QString &fileName, pluginsFiles)
+          {
+              QPluginLoader pluginLoader(pluginsDir.absoluteFilePath(fileName));
+
+              if(!pluginLoader.load())
+                         continue;
+
+              QObject *pluginObject = qobject_cast<QObject*>(pluginLoader.instance());
+
+              if(pluginObject)
+              {
+                 const QMetaObject *metaObject = pluginObject->metaObject();
+                 int index = metaObject->indexOfClassInfo("Dependencies");
+
+                 if(index != -1)
+                 {
+                     QMetaClassInfo classInfo = metaObject->classInfo(index);
+                     QString dependenciesString = classInfo.value();
+
+                     QStringList dependenciesList = dependenciesString.split(";");
+
+                     foreach(QString dependencyName, dependenciesList)
+                     {
+                         QObject *dependencyObject = QCoreApplication::instance()->findChild<QObject*>(dependencyName);
+                         if(dependencyObject)
+                         {
+                             dependencies.append(QPointer<QObject>(dependencyObject));
+                         }
+                     }
+
+                 }
+              }
+
+              BaseInterface *interface = qobject_cast<BaseInterface*>(pluginObject);
+              if(interface)
+              {
+                  interface->initialize(dependencies);
+              }
+
+          }
+    }
 }

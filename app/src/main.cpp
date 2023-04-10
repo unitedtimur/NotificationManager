8106dd886962f5a4c5d1905c01725867d32bff5a
Anton <inamometr1@gmail.com>
2023-04-10 17:17:23 +0700

    separate qml files, and notify_model

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,23 +2,10 @@
 #include "core.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "notify_model.h"
 #include <QQmlEngine>
 #include <QQmlContext>
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-    QGuiApplication app(argc, argv);
-    NotificationModel notificationModel;
-    // Добавляем уведомление в модель
-    notificationModel.addNotification("Обнаружена цель в SportCenter", "Очень важная информация. Очень важная информация.", 0);
-    notificationModel.addNotification("Камера не доступна!", "Очень важная информация. Очень важная информация.", 1);
-    notificationModel.addNotification("Высокий уровень шума", "Очень важная информация. Очень важная информация.", 2);
-    QQmlApplicationEngine engine;
-    engine.rootContext()->setContextProperty("Notificationmodel", &notificationModel);
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
-    return app.exec();
+    Core::init();
+    return 0;
 }
--- plugins/logic/include/logic_plugin_export.h
+++ plugins/logic/include/logic_plugin_export.h
@@ -2,13 +2,41 @@
 #ifndef LOGIC_PLUGIN_EXPORT_H
 #define LOGIC_PLUGIN_EXPORT_H
 
-#include <QtGlobal>
-
-#ifdef LOGIC_PLUGIN
-#    define LOGIC_PLUGIN_EXPORT Q_DECL_EXPORT
+#ifdef LOGIC_PLUGIN_STATIC_DEFINE
+#  define LOGIC_PLUGIN_EXPORT
+#  define LOGIC_PLUGIN_NO_EXPORT
 #else
-#    define LOGIC_PLUGIN_EXPORT Q_DECL_IMPORT
+#  ifndef LOGIC_PLUGIN_EXPORT
+#    ifdef logic_plugin_EXPORTS
+        /* We are building this library */
+#      define LOGIC_PLUGIN_EXPORT __declspec(dllexport)
+#    else
+        /* We are using this library */
+#      define LOGIC_PLUGIN_EXPORT __declspec(dllimport)
+#    endif
+#  endif
+
+#  ifndef LOGIC_PLUGIN_NO_EXPORT
+#    define LOGIC_PLUGIN_NO_EXPORT 
+#  endif
 #endif
 
+#ifndef LOGIC_PLUGIN_DEPRECATED
+#  define LOGIC_PLUGIN_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef LOGIC_PLUGIN_DEPRECATED_EXPORT
+#  define LOGIC_PLUGIN_DEPRECATED_EXPORT LOGIC_PLUGIN_EXPORT LOGIC_PLUGIN_DEPRECATED
+#endif
+
+#ifndef LOGIC_PLUGIN_DEPRECATED_NO_EXPORT
+#  define LOGIC_PLUGIN_DEPRECATED_NO_EXPORT LOGIC_PLUGIN_NO_EXPORT LOGIC_PLUGIN_DEPRECATED
+#endif
+
+#if 0 /* DEFINE_NO_DEPRECATED */
+#  ifndef LOGIC_PLUGIN_NO_DEPRECATED
+#    define LOGIC_PLUGIN_NO_DEPRECATED
+#  endif
+#endif
 
 #endif /* LOGIC_PLUGIN_EXPORT_H */

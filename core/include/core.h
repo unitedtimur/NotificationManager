8b45aea5b6ea0daee6fe8bbe744c811052292e82
MrKarls <mrkarls0077@gmail.com>
2023-04-11 13:25:02 +0700

    add Core class,fix error processing

--- core/include/core.h
+++ core/include/core.h
@@ -1,12 +1,31 @@
 #pragma once
+#include "base_interface.h"
+#include <QObject>
 #include <iostream>
 #include <QString>
 #include <QDir>
+#include <QMessageBox>
 #include <QPluginLoader>
 #include <QMetaClassInfo>
 #include <QCoreApplication>
-#include "base_interface.h"
+
 namespace Core {
-    void init();
-    void loadPlugins();
+    /*!
+    * \brief Класс, реализующий основной функционал ядра
+    */
+    class CoreBasis:public QObject
+    {
+        Q_OBJECT
+
+    public:
+        /*!
+         * \brief Конструктор класса CoreBasis
+         */
+        explicit CoreBasis();
+        /*!
+         * \brief Функция загрузки плагинов
+         * Данная функция производит поиск и загрузку плагинов
+         */
+        void loadPlugins();
+};
 }

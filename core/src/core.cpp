8858ec5e15ddb4e2dfd3ca00f32cb01705eaf4b6
MrKarls <mrkarls0077@gmail.com>
2023-04-12 17:09:37 +0700

    class full refactoring

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -1,69 +1,61 @@
 #include "core.h"
-#include <iostream>
 
 namespace Core {
-    CoreBasis::CoreBasis()
+    void CoreBasis::loadPlugins(const QString &path)
     {
-        std::cout << "Core init" << std::endl;
-    }
+        QStringList plugins;
+        QDir dir(path);
 
-    void CoreBasis::loadPlugins()
-    {
-#ifdef Q_OS_WIN64
-        const auto pluginExtension = ".dll";
-#elif defined(Q_OS_LINUX)
-        const auto pluginExtension = ".so";
+        if (dir.exists())
+#ifdef Q_OS_WIN
+            plugins = dir.entryList(QStringList("*.dll"), QDir::Files);
+#elif defined Q_OS_UNIX
+            plugins = dir.entryList(QStringList("*.so"), QDir::Files);
 #endif
-        QString parentDir(QDir::currentPath());
-        QDir currentDir(parentDir);
-        currentDir.cdUp();
-        QString projectDir = currentDir.absolutePath();
 
-        QDir pluginsDir(projectDir);
-        pluginsDir.cd("plugins");
+        for (const auto &plugin : qAsConst(plugins)) {
+            QPluginLoader pluginLoader(dir.absolutePath() + "/" + plugin);
 
-        if (!pluginsDir.exists()) {
-            QMessageBox::critical(nullptr, "Error", "Directories do not exist");
-            exit(EXIT_FAILURE);
+            const auto pluginInterface = pluginLoader.instance();
+
+            if (!pluginInterface) {
+                qWarning() << "Unknow plugin in plugins folder";
+                continue;
+            }
+
+            if (const auto plugin = qobject_cast<BaseInterface *>(pluginInterface); plugin) {
+                _plugins.push_back(plugin);
+            }
         }
 
-        QStringList pluginsFiles =
-         pluginsDir.entryList(QStringList() << pluginExtension, QDir::Files);
+        const auto excludePlugin = [this](auto excludedPlugin, auto plugins) {
+            QList<QPointer<QObject>> result;
 
-        QList<QPointer<QObject>> dependencies;
-
-        foreach (const QString &fileName, pluginsFiles) {
-            QPluginLoader pluginLoader(pluginsDir.absoluteFilePath(fileName));
-
-            if (!pluginLoader.isLoaded())
-                continue;
-
-            QObject *pluginObject = qobject_cast<QObject *>(pluginLoader.instance());
-
-            if (pluginObject) {
-                const QMetaObject *metaObject = pluginObject->metaObject();
-                int index = metaObject->indexOfClassInfo("Dependencies");
-
-                if (index != -1) {
-                    QMetaClassInfo classInfo = metaObject->classInfo(index);
-                    QString dependenciesString = classInfo.value();
-
-                    QStringList dependenciesList = dependenciesString.split(";");
-
-                    foreach (QString dependencyName, dependenciesList) {
-                        QObject *dependencyObject =
-                         QCoreApplication::instance()->findChild<QObject *>(dependencyName);
-                        if (dependencyObject) {
-                            dependencies.append(QPointer<QObject>(dependencyObject));
-                        }
-                    }
+            for (const auto &plugin : plugins) {
+                if (plugin != excludedPlugin) {
+                    result.append(plugin);
                 }
             }
 
-            BaseInterface *interface = qobject_cast<BaseInterface *>(pluginObject);
-            if (interface) {
-                interface->initialize(dependencies);
+            return result;
+        };
+
+        for (const auto &plugin : qAsConst(_plugins)) {
+            auto *interface = qobject_cast<BaseInterface *>(plugin);
+            const auto isLoaded = interface->initialize(excludePlugin(plugin, _plugins));
+
+            if (!isLoaded) {
+                qWarning() << "Failed to load plugin";
+                _plugins = excludePlugin(plugin, _plugins);
+            } else {
+                qInfo() << "Plugin is loaded";
             }
         }
     }
+
+    QPointer<QQmlApplicationEngine> CoreBasis::qmlEngine() const
+    {
+        return _qmlEngine;
+    }
+
 } // namespace Core

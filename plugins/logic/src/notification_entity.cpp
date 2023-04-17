ceeda8929cac5afcb86f27eae56db275401993e7
Anton <inamometr1@gmail.com>
2023-04-17 17:25:40 +0700

    naming fixes

--- plugins/logic/src/notification_entity.cpp
+++ plugins/logic/src/notification_entity.cpp
@@ -1,4 +1,4 @@
 #include "notification_entity.h"
 
-LogicPlugin::notification_entity::notification_entity(QString title, QString description, int type): AbstractNotification(title,  description, type)
+LogicPlugin::NotificationEntity::NotificationEntity(QString title, QString description, int type): AbstractNotification(title,  description, type)
 {}

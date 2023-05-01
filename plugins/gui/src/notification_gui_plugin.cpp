d9b0499d2147083ee59e74fe78b0b8ee510e1eb2
Kirill Bryntsev <ki.b2310111@gmail.com>
2023-05-01 19:14:40 +0700

    add namespace in cpp files of plugins

--- plugins/gui/src/notification_gui_plugin.cpp
+++ plugins/gui/src/notification_gui_plugin.cpp
@@ -2,14 +2,15 @@
 
 #include <QDebug>
 
-bool GuiPlugin::NotificationGuiPlugin::initialize(const QList<QPointer<QObject>> &dependencies)
-{
-    if (Core::AbstractCore::findPlugin<LogicPlugin::NotificationLogicPlugin>(_logicPlugin,
-                                                                             dependencies)) {
-        qDebug() << Q_FUNC_INFO << "Plugin dependencies found";
-        return true;
-    } else {
-        qWarning() << Q_FUNC_INFO << "Plugin dependencies not found";
-        return false;
+namespace GuiPlugin {
+    bool NotificationGuiPlugin::initialize(const QList<QPointer<QObject>> &dependencies)
+    {
+        if (Core::AbstractCore::findPlugin(_logicPlugin, dependencies)) {
+            qDebug() << Q_FUNC_INFO << "Plugin dependencies found";
+            return true;
+        } else {
+            qWarning() << Q_FUNC_INFO << "Plugin dependencies not found";
+            return false;
+        }
     }
 }

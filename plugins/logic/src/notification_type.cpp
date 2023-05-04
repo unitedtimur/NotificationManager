48ca818762690dbb9373aa2ccd01a70ed7cacc11
Anton <inamometr1@gmail.com>
2023-05-05 04:19:48 +0700

    fix

--- plugins/logic/src/notification_type.cpp
+++ plugins/logic/src/notification_type.cpp
@@ -9,41 +9,35 @@ std::optional<uint16_t> LogicPlugin::NotificationType::addType(const QString &na
     if (name.isEmpty())
         return std::nullopt;
     const auto it = std::find_if(_types.begin(), _types.end(),
-                                 [&name](const std::pair<QString, uint16_t> &element) {
+                                 [&name](const auto &element) {
                                      return element.first == name;
                                  });
-    if (it != _types.end()) { // если тип уедомления с названием name уже существует, то мы ничего
-                              // не добавляем в структуру
+    if (it != _types.end()) // если тип уедомления с названием name уже существует, то мы ничего
+                            // не добавляем в структуру
         return std::nullopt;
-    } else {
-        ++_nTypes;
-        _types[name] = _nTypes;
-        return { it->second };
-    }
+    ++_nTypes;
+    _types[name] = _nTypes;
+    return { it->second };
 }
 
 const QString LogicPlugin::NotificationType::stringType(const uint16_t &value)
 {
     const auto it = std::find_if(_types.begin(), _types.end(),
-                                 [&value](const std::pair<QString, uint16_t> &element) {
+                                 [&value](const auto &element) {
                                      return element.second == value;
                                  });
-    if (it != _types.end()) {
+    if (it != _types.end())
         return it->first;
-    } else {
-        return "";
-    }
+    return "";
 }
 
 bool LogicPlugin::NotificationType::isTypeExist(const uint16_t &value)
 {
     const auto it = std::find_if(_types.begin(), _types.end(),
-                                 [&value](const std::pair<QString, uint16_t> &element) {
+                                 [&value](const auto &element) {
                                      return element.second == value;
                                  });
-    if (it != _types.end()) {
+    if (it != _types.end())
         return true;
-    } else {
-        return false;
-    }
+    return false;
 }

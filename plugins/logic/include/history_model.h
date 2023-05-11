bfc5dc74b83ba85fbe5c5eb3c36506bee7868342
goshantiy <goshantiy@gmail.com>
2023-05-12 01:15:01 +0700

    namespace+little fixes

--- plugins/logic/include/history_model.h
+++ plugins/logic/include/history_model.h
@@ -2,22 +2,23 @@
 #define HISTORY_MODEL_H
 #include <QSqlTableModel>
 #include <QSqlRecord>
-
-class HistoryModel : public QSqlTableModel
-{
-    Q_OBJECT
-public:
-    enum Roles {
-        TitleRole = Qt::UserRole + 1,
-        MessageRole,
-        TypeRole,
-        DateRole
+namespace LogicPlugin {
+    class HistoryModel final: public QSqlTableModel
+    {
+        Q_OBJECT
+    public:
+        enum Roles
+        {
+            TitleRole = Qt::UserRole + 1,
+            MessageRole,
+            TypeRole,
+            DateRole
+        };
+        HistoryModel(QObject *parent = nullptr);
+        QHash<int, QByteArray> roleNames() const override;
+        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+        Q_INVOKABLE void selectByDate(const QString &dateStr);
     };
-    HistoryModel(QObject *parent = nullptr);
-    QHash<int, QByteArray> roleNames() const override;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    Q_INVOKABLE void selectByDate(const QString& dateStr);
-};
-
+}
 
 #endif // HISTORY_MODEL_H
--- plugins/logic/src/history_model.cpp
+++ plugins/logic/src/history_model.cpp
@@ -1,44 +1,43 @@
 #include "history_model.h"
-namespace LogicPlugin{
-HistoryModel::HistoryModel(QObject *parent = nullptr)
-    : QSqlTableModel(parent)
-{
-    setTable("notification");
-    select();
-}
-
-QHash<int, QByteArray> HistoryModel::roleNames() const override
-{
-    QHash<int, QByteArray> roles;
-    roles[TitleRole] = "title";
-    roles[MessageRole] = "message";
-    roles[TypeRole] = "type";
-    roles[DateRole] = "date";
-    return roles;
-}
-
-QVariant HistoryModel::data(const QModelIndex &index, int role = Qt::DisplayRole) const override
-{
-    if (role < Qt::UserRole)
-        return QSqlTableModel::data(index, role);
-
-    QSqlRecord record = QSqlTableModel::record(index.row());
-    switch (role) {
-    case TitleRole:
-        return record.value("Title");
-    case MessageRole:
-        return record.value("Description");
-    case TypeRole:
-        return record.value("Type");
-    case DateRole:
-        return record.value("Date");
-    default:
-        return QVariant();
+namespace LogicPlugin {
+    HistoryModel::HistoryModel(QObject *parent) : QSqlTableModel(parent)
+    {
+        setTable("notification");
+        select();
     }
-}
-void HistoryModel::selectByDate(const QString& dateStr)
-{
-    setFilter(QString("Date='%1'").arg(dateStr));
-    select();
-}
+
+    QHash<int, QByteArray> HistoryModel::roleNames() const
+    {
+        QHash<int, QByteArray> roles;
+        roles[TitleRole] = "title";
+        roles[MessageRole] = "message";
+        roles[TypeRole] = "type";
+        roles[DateRole] = "date";
+        return roles;
+    }
+
+    QVariant HistoryModel::data(const QModelIndex &index, int role) const
+    {
+        if (role < Qt::UserRole)
+            return QSqlTableModel::data(index, role);
+
+        QSqlRecord record = QSqlTableModel::record(index.row());
+        switch (role) {
+        case TitleRole:
+            return record.value("Title");
+        case MessageRole:
+            return record.value("Description");
+        case TypeRole:
+            return record.value("Type");
+        case DateRole:
+            return record.value("Date");
+        default:
+            return QVariant();
+        }
+    }
+    void HistoryModel::selectByDate(const QString &dateStr)
+    {
+        setFilter(QString("Date='%1'").arg(dateStr));
+        select();
+    }
 }
